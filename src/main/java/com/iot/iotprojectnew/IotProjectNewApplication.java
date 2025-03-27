ddd8f6a7e7d111189207e8f60b2782aba2a50472
JustHungDEVer <hungtxhe194146.dev@gmail.com>
2025-03-28 01:03:29 +0700

    init iot project

---
+++ src/main/java/com/iot/iotprojectnew/IotProjectNewApplication.java
@@ -0,0 +1,13 @@
+package com.iot.iotprojectnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotProjectNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotProjectNewApplication.class, args);
+    }
+
+}
